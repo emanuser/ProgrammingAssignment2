ee0940c661574f1d049afddd85056cfd4ac8c73d
emanuser <tdolgos@gmail.com>
2015-09-27 18:51:14 -0400

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions caclculate, cash, and retrieve the inverse of a square matrix
+## Both are based off of the example functions given in the assignment description
 
-## Write a short comment describing this function
+
+## This function chaches and calculates a given matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  reset_matrix <- function(y){
+    x <<- y
+    m <<- solve(x)
+  }
+  retrn_matrix <- function() x 
+  solve_matrix <- function() m <<- solve(x)
+  solve_matrix()
+  get_inverse <- function() m 
+  list(retrn_matrix = retrn_matrix, reset_matrix = reset_matrix,
+       solve_matrix = solve_matrix,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+
+##  This function returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$get_inverse()
+  if(!is.null(m)) {
+    message("cached, getting data")
+    return(m)
+  }
+  if(is.null(m)) {
+    message("Not cached, solving !")
+    m <- x$solve_matrix()
+    return(m)
+  }
+  
 }
+
+
+
+## Example inputs
+
+## Store the matrix and its inverse in object 'a'
+#a <- makeCacheMatrix(matrix(c(3,5,7,9),2,2))
+
+##  Retrieve cached inverse of object 'a'
+#cacheSolve(a)  
+
+## Return matrix stored in object 'a'
+#a$retrn_matrix()
+
+## if(matrix is solved), Returns cached inverse in object 'a'
+#a$get_inverse()
+
+# Test new square matrix
+#a$reset_matrix(matrix(c(2,9,4,8),2,2))
+
+## To see message "Not cached, solving !", hashtag solve_matrix() to look like this 
+#solve_matrix()
